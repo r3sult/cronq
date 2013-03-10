d0b20056927ff9927cea75b577365de30fcffd15
Philip Cristiano <philipcristiano@gmail.com>
2013-03-10 14:57:58 -0400

    v0.0.21

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 def run_setup():
     setup(
         name='cronq',
-        version='0.0.20',
+        version='0.0.21',
         description='A Cron-like system for running tasks',
         keywords = 'cron amqp',
         url='http://github.com/philipcristiano/cronq',
