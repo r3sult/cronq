10ed5047316c12b18fe7c6e0d11d5988bf9daeb5
zackkitzmiller <zackkitzmiller@gmail.com>
2014-01-02 10:38:12 -0500

    bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 def run_setup():
     setup(
         name='cronq',
-        version='0.0.25',
+        version='0.0.26',
         description='A Cron-like system for running tasks',
         keywords = 'cron amqp',
         url='http://github.com/philipcristiano/cronq',
