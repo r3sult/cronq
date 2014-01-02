c557acc4d3803b654a54eb62838e6a000e6dc67b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-01-02 14:39:26 -0500

    Release version 0.0.27

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 def run_setup():
     setup(
         name='cronq',
-        version='0.0.26',
+        version='0.0.27',
         description='A Cron-like system for running tasks',
         keywords = 'cron amqp',
         url='http://github.com/philipcristiano/cronq',
