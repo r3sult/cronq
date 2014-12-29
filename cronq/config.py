ae823148bb8dd7696ab3ab6c2b735d15abe314a0
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-12-29 16:15:00 -0500

    Add a config.py module to contain configuration for the entire app

---
+++ cronq/config.py
@@ -0,0 +1,18 @@
+import os
+import random
+
+
+if os.getenv('RABBITMQ_HOSTS', None) is not None:
+    hosts = os.getenv('RABBITMQ_HOSTS', None).split(',')
+    random.shuffle(hosts)
+else:
+    hosts = [os.getenv('RABBITMQ_HOST', 'localhost')]
+
+DATABASE_URL = os.getenv(
+    'CRONQ_MYSQL',
+    'mysql+mysqlconnector://root@localhost/cronq')
+LOG_PATH = os.getenv('LOG_PATH', '/var/log/cronq')
+QUEUE = os.getenv('CRONQ_QUEUE', 'cronq_jobs')
+RABBITMQ_HOSTS = hosts
+RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
+RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASS', 'guest')
