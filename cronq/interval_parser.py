c557abf8702f52bd2e9c13e244ad90516da7ba6c
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-10-10 13:01:32 -0400

    Add source code encodings to all python files

--- cronq/interval_parser.py
+++ cronq/interval_parser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import datetime
 import aniso8601
 
--- cronq/models/category.py
+++ cronq/models/category.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from sqlalchemy import Column
 from sqlalchemy import CHAR
 from sqlalchemy import Integer
