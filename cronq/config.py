42da713347af7976eefc38f26d5139def4f7dbd8
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2017-11-21 15:26:56 -0500

    refactor: drop extra configuration logic

--- cronq/config.py
+++ cronq/config.py
@@ -1,35 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
-import random
 
-
-if os.getenv('RABBITMQ_HOSTS', None) is not None:
-    hosts = os.getenv('RABBITMQ_HOSTS', None).split(',')
-    random.shuffle(hosts)
-else:
-    hosts = [os.getenv('RABBITMQ_HOST', 'localhost')]
-
-default_log_path = '/var/log/cronq'
-if not os.path.exists('/var/log/cronq'):
-    default_log_path = '/tmp'
 
 DATABASE_URL = os.getenv(
     'DATABASE_URL',
     os.getenv('CRONQ_MYSQL',
-              'mysql+pymysql://root@localhost/cronq'))
-if DATABASE_URL.startswith('mysql://'):
-    DATABASE_URL = 'mysql+pymysql://' + DATABASE_URL[8:]
+              'mysql+pymysql://root@localhost/cronq')).replace(
+                    'mysql://', 'mysql+pymysql://')
 
-LOG_PATH = os.getenv('LOG_PATH', default_log_path)
+LOG_PATH = os.getenv('LOG_PATH', '/var/log/cronq') if not os.path.exists('/var/log/cronq') else '/tmp'  # noqa
 QUEUE = os.getenv('CRONQ_QUEUE', 'cronq_jobs')
-RABBITMQ_HOSTS = hosts
-RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
-RABBITMQ_PASS = os.getenv('RABBITMQ_PASS', 'guest')
-
-default_rabbitmq_url = 'amqp://{0}:{1}@{2}/'.format(
-    RABBITMQ_USER,
-    RABBITMQ_PASS,
-    ','.join(RABBITMQ_HOSTS)
-)
-
-RABBITMQ_URL = os.getenv('RABBITMQ_URL', default_rabbitmq_url)
+RABBITMQ_URL = os.getenv('RABBITMQ_URL', 'amqp://guest:guest@localhost/')
